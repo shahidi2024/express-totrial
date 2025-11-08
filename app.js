011c39350912d6de29d7fe9fcb292783f476d859
Jack-lib <jack@gmail.com>
2025-11-08 16:32:02 +0330

    crud action with Express

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
-const express = require('express')
-const app = express()
-const port = 5000
-
+const express = require("express");
+const app = express();
+const port = 5000;
 
 const courses = [
-    {
-        id : 1,
-        title : 'JS',
-        price : 0
-    },
-    {
-        id : 2,
-        title : 'React',
-        price : 5000
-    },
-    {
-        id : 3,
-        title : 'Nodejs',
-        price : 3500
-    },
-    {
-        id : 4,
-        title : 'MongoDB',
-        price : 0
-    },
-]
+  {
+    id: 1,
+    title: "JS",
+    price: 0,
+  },
+  {
+    id: 2,
+    title: "React",
+    price: 5000,
+  },
+  {
+    id: 3,
+    title: "Nodejs",
+    price: 3500,
+  },
+  {
+    id: 4,
+    title: "MongoDB",
+    price: 0,
+  },
+];
 
-app.get('/courses/:courseId', (req, res) => {
+app.get("/courses/:courseId", (req, res) => {
+  const course = courses.find(
+    (item) => item.id === Number(req.params.courseId)
+  );
+  if (course) {
+    res.json(course);
+  } else {
+    res.send("no course found :))");
+  }
+});
 
-    const course = courses.find( item => item.id === Number(req.params.courseId) )
-    if (course) {
-        res.json(course)
-    } else {
-        res.send('no course found :))')
-    }
 
-  
+app.post('/courses', ( req, res ) => {
+    res.statusCode = 201;
+    res.send(`New Courses Created Successfully`)
+})
+
+app.delete('/courses/:id', ( req, res ) => {
+    res.status(200).send('Main Courses Removed Successfully')
+})
+
+app.put('/courses/:id', ( req, res ) => {
+    res.status(401).send('Main Courses Updated Successfully')
 })
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
